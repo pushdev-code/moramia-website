9657ec204e829f6aa9b49bd8124ef57630100078
Diego <diego.mateus@javeriana.edu.co>
2020-05-22 01:31:38 -0500

    Added the funcionality of hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import {Product} from './modules/product.js';
 import './assets/css/main.scss'
 
+const hamburgerMenu = document.querySelector('.mobile');
+const close = document.querySelector('.close');
+var header = document.querySelector('.desktop');
+
 console.log(new Product('almond',4150));
+
+hamburgerMenu.addEventListener('click',()=>{
+    header.style.left = "0px";
+});
+
+close.addEventListener('click',()=>{
+    //header.style.left = "-320px";
+});
+
