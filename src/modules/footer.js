d40e64ce47e98fefdb780e948fd335fd267ab47e
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-06-04 14:37:06 -0500

    Fixes minor bug

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -2,7 +2,7 @@ const footerList = document.querySelector(".footer-ulist");
 const footerLinks = footerList.querySelectorAll("a");
 for (let i = 0; i < footerLinks.length; i++) {
   footerLinks[i].addEventListener("click", () => {
-    let current = document.querySelector(".active");
+    let current = footerList.querySelector(".active");
     current.className = "footer-a";
     footerLinks[i].className = "active";
   });
