f506803ec674e17245c0718d1a4957b3b01ea0ee
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-06-03 21:13:55 -0500

    Resolves pr issues

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import {Product} from './modules/product.js';
 import './assets/css/main.scss'
 
 console.log(new Product('almond',4150));
+
+const footerList = document.querySelector('.footer-ulist');
+const footerLinks = footerList.querySelectorAll('a');
+for(let i = 0; i < footerLinks.length; i++){
+    footerLinks[i].addEventListener('click',()=>{
+        let current = document.querySelector('.active');
+        current.className = "footer-a";
+       footerLinks[i].className = "active";
+    });
+}
