6ce9fc29310629d34485b37caf3ddc7a3c0332bd
Diego <diego.mateus@javeriana.edu.co>
2020-06-03 21:28:35 -0500

    More readable code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import {Product} from './modules/product.js';
 import './assets/css/main.scss'
 
-const hamburgerMenu = document.querySelector('.mobile');
+const hamburgerMenu = document.querySelector('.mor-mobile');
 const close = document.querySelector('.close');
-var header = document.querySelector('.desktop');
+var header = document.querySelector('.mor-desktop');
 
 console.log(new Product('almond',4150));
 
 hamburgerMenu.addEventListener('click', ()=>{
-    if(header.classList.value.toString() === 'desktop desktop-open'){
+    if(header.classList.value.toString() === 'mor-desktop desktop-open'){
         header.classList.remove('desktop-open');
         header.classList.add('desktop-close');
     }
