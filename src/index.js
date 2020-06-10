45002949460b615951abdbe266ae4978a981a62d
Diego <diego.mateus@javeriana.edu.co>
2020-06-10 11:22:11 -0500

    Correct the names of the classes and errase the wrong atributes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import {Product} from './modules/product.js';
 import './assets/css/main.scss'
 
-const hamburgerMenu = document.querySelector('.mor-mobile');
+const hamburgerMenu = document.querySelector('.mor-menu-container');
 const close = document.querySelector('.close');
-var header = document.querySelector('.mor-desktop');
+var header = document.querySelector('.mor-links-container');
 
 console.log(new Product('almond',4150));
 
 hamburgerMenu.addEventListener('click', ()=>{
-    if(header.classList.value.toString() === 'mor-desktop desktop-open'){
-        header.classList.remove('desktop-open');
-        header.classList.add('desktop-close');
+    if(header.classList.value.toString() === 'mor-links-container open'){
+        header.classList.remove('open');
+        header.classList.add('close');
     }
     else {
-        header.classList.add('desktop-open');
-        header.classList.remove('desktop-close');
+        header.classList.add('open');
+        header.classList.remove('close');
     }
 });
 
 close.addEventListener('click', ()=>{
-    header.classList.remove('desktop-open');
-    header.classList.add('desktop-close');
+    header.classList.remove('open');
+    header.classList.add('close');
 });
 
