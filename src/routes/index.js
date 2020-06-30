19ec4c8eb0a9c2536fda48ac18c6ae32a7347011
Diego Mateus <diego.mateus@javeriana.edu.co>
2020-06-30 16:42:16 -0500

    Include the correct structure and correct the image in the footer

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,29 +15,4 @@ router.get('/', async (req,res) => {
     }
 });
 
-async function product(){
-    const prd = await Products.find();
-    for(let i = 0; i < prd.length; i++){
-        router.get('/product/' + prd[i].product_name, async(req, res) => {
-            res.render('pages/product', {
-                title: 'Moramia ' + prd[i].product_name + ' <3',
-                id: prd[i].productId,
-                nombre: prd[i].product_name,
-                precio: prd[i].product_price,
-                imagen: prd[i].product_image,
-                imagen2: prd[i].product_image2,
-                descripcion: prd[i].product_description,
-                galeria1: prd[i].product_gallery.image1,
-                galeria2: prd[i].product_gallery.image2,
-                galeria3: prd[i].product_gallery.image3,
-                galeria4: prd[i].product_gallery.image4,
-                peso: prd[i].product_weight,
-                existencia: prd[i].product_in_stock,
-                refreshUrl: process.env.BROWSER_REFRESH_URL //browser refresh listening
-            });
-        });
-    }  
-}
-
-product();
 module.exports = router;
