d983156fe45041b5342bb152d5c9deb262c163a2
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-07-03 17:37:24 -0500

    Resolves pr issues

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,10 +4,10 @@ const Products = require('../models/Products');
 
 router.get('/:name', async (req,res) => {
     try {
-        const prd = await Products.findOne({product_name : req.params.name});
+        const product = await Products.findOne({product_name : req.params.name});
        res.render("pages/product", {
             title: "Moramia <3",
-            prd : prd,
+            product,
             refreshUrl: process.env.BROWSER_REFRESH_URL, //browser refresh listening
           });
     }catch(err){
--- src/static/js/modules/gallery.js
+++ src/static/js/modules/gallery.js
@@ -4,39 +4,43 @@ function imageGallery() {
     const previews = document.querySelectorAll(".gallery-img");
     const reset = document.querySelector(".gallery-wrapper-miniture img")
 
-    previews.forEach((preview) => {
+    for(const preview of previews) {
         preview.addEventListener("click", function () {
             let size = carouselSlide[0].clientWidth;
             let counter = this.id;
-            previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
+            previews.forEach((p) => p.classList.remove(ACTIVE_CLASS));
             preview.classList.add(ACTIVE_CLASS);
             carouselSlide.forEach((cl) => {
-                cl.style.transform = "translateX(" + -size * counter + "px)";
+                cl.style.transform = "translateX(" + calculateMovement(size,counter) + "px)";
             });
 
         });
         preview.addEventListener("mouseover", function () {
             const size = carouselSlide[0].clientWidth;
             let counter = this.id;
-            previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
+            previews.forEach((p) => p.classList.remove(ACTIVE_CLASS));
             preview.classList.add(ACTIVE_CLASS);
             carouselSlide.forEach((cl) => {
-                cl.style.transform = "translateX(" + -size * counter + "px)";
+                cl.style.transform = "translateX(" + calculateMovement(size,counter) + "px)";
             });
 
         });
-    });
+    };
 
     //Resets slider
     window.addEventListener('resize', function (event) {
         let size = carouselSlide[0].clientWidth;
         let counter = reset.id;
-        previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
+        previews.forEach((p) => p.classList.remove(ACTIVE_CLASS));
         reset.classList.add(ACTIVE_CLASS);
         carouselSlide.forEach((cl) => {
-            cl.style.transform = "translateX(" + -size * counter + "px)";
+            cl.style.transform = "translateX(" + calculateMovement(size,counter) + "px)";
         });
     });
+}
+
+function calculateMovement(size, counter){
+    return -size * counter;
 }
 
 imageGallery();
