cf9abac1da7c240bdb2e085b82f901f9f07329fa
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-07-03 05:12:51 -0500

    Resolves slider animation

--- src/static/js/modules/gallery.js
+++ src/static/js/modules/gallery.js
@@ -1,21 +1,42 @@
 function imageGallery() {
-    const ACTIVE_CLASS = "active"; 
-    const highlight = document.querySelector('.gallery-highlight');
-    const previews = document.querySelectorAll('.gallery-img');
+    const ACTIVE_CLASS = "active";
+    const carouselSlide = document.querySelectorAll(".item-slide");
+    const previews = document.querySelectorAll(".gallery-img");
+    const reset = document.querySelector(".gallery-wrapper-miniture img")
 
-    previews.forEach(preview => {
-        preview.addEventListener('click', function () {
-            highlight.src = this.src;
-            previews.forEach(preview => preview.classList.remove(ACTIVE_CLASS));
+    previews.forEach((preview) => {
+        preview.addEventListener("click", function () {
+            let size = carouselSlide[0].clientWidth;
+            let counter = this.id;
+            previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
             preview.classList.add(ACTIVE_CLASS);
+            carouselSlide.forEach((cl) => {
+                cl.style.transform = "translateX(" + -size * counter + "px)";
+            });
+
         });
-        preview.addEventListener('mouseover', function () {
-            highlight.src = this.src;
-            previews.forEach(preview => preview.classList.remove(ACTIVE_CLASS));
+        preview.addEventListener("mouseover", function () {
+            const size = carouselSlide[0].clientWidth;
+            let counter = this.id;
+            previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
             preview.classList.add(ACTIVE_CLASS);
+            carouselSlide.forEach((cl) => {
+                cl.style.transform = "translateX(" + -size * counter + "px)";
+            });
+
+        });
+    });
+
+    //Resets slider
+    window.addEventListener('resize', function (event) {
+        let size = carouselSlide[0].clientWidth;
+        let counter = reset.id;
+        previews.forEach((preview) => preview.classList.remove(ACTIVE_CLASS));
+        reset.classList.add(ACTIVE_CLASS);
+        carouselSlide.forEach((cl) => {
+            cl.style.transform = "translateX(" + -size * counter + "px)";
         });
     });
 }
-
 
 imageGallery();
