af4f5ab75074c00aa44a910dd7eab65dfbbe80ee
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-07-06 18:03:10 -0500

    Resolves lower case issue

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,16 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Products = require('../models/Products');
 
-router.get('/:name', async (req,res) => {
+router.get('/:name', async (req, res) => {
     try {
-        const product = await Products.findOne({product_name : req.params.name});
-       res.render("pages/product", {
+        const product = await Products.findOne({
+            url_slug: req.params.name.toLowerCase()
+        });
+        if (!product) return res.status(404).send('Not found');
+        res.render("pages/product", {
             title: "Moramia <3",
             product,
             refreshUrl: process.env.BROWSER_REFRESH_URL, //browser refresh listening
-          });
-    }catch(err){
-        res.json({message : err});
+        });
+    } catch (err) {
+       console.log(err);
+       res.status(500).send(err);
     }
 });
 
