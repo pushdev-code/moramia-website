c87ca0730bd9c8508bc5f993a1c05ef58e8e936b
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-07-07 20:31:16 -0500

    Resolves pr issues

--- src/static/js/modules/gallery.test.js
+++ src/static/js/modules/gallery.test.js
@@ -2,22 +2,22 @@ const functions = require('./gallery.js');
 
 
 describe('calculateMovement test', () => {
-    test('Sees if calculations are done properly', () => {
+    test('should return a negative value calculation when the counter passed ', () => {
         expect(functions.calculateMovement(780, 2)).toBe(-1560);
     })
-    test('Sees if calculations are done properly', () => {
+    test('should return 0 if counter is 0', () => {
         expect(functions.calculateMovement(1200, 0)).toBe(0);
     })
-    test('Sees if calculations are done properly', () => {
+    test('should return a negative value calculation when the counter passed', () => {
         expect(functions.calculateMovement(782.35, 3)).toBe(-2347.05);
     })
-    test('Sees that the calculation is taking into account the negative', () => {
+    test('Should not return a positive number', () => {
         expect(functions.calculateMovement(780, 2)).not.toBe(1560);
     })
-    test('Sees what happens if undefined values ​​arrive  ', () => {
+    test('Should return NaN if the size is not numeric ', () => {
         expect(functions.calculateMovement(undefined, 3)).toBeNaN();
     })
-    test('Sees what happens if string values ​​arrive  ', () => {
+    test('Should return NaN if the counter is not numeric ', () => {
         expect(functions.calculateMovement(159, "test")).toBeNaN();
     })
 })
