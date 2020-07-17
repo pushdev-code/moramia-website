b27eb52fe632ef7798bf390894820bf3f37ee87e
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-07-17 16:52:47 -0500

    Adds accessibility semantic

--- src/static/js/modules/gallery.js
+++ src/static/js/modules/gallery.js
@@ -1,17 +1,20 @@
+
 function imageGallery() {
     const ACTIVE_CLASS = "active";
     const carouselSlide = document.querySelectorAll(".item-slide");
     const previews = document.querySelectorAll(".gallery-img");
-    const reset = document.querySelector(".gallery-wrapper-miniture img")
+    const reset = document.querySelector(".gallery-wrapper-miniture img");
 
     for (const preview of previews) {
-        preview.addEventListener("click", function () {
-            let size = carouselSlide[0].clientWidth;
-            let counter = this.id;
-            previews.forEach((p) => p.classList.remove(ACTIVE_CLASS));
-            preview.classList.add(ACTIVE_CLASS);
-            carouselSlide.forEach((cl) => {
-                cl.style.transform = "translateX(" + calculateMovement(size, counter) + "px)";
+        ["keypress", "click"].forEach(evt =>{
+            preview.addEventListener(evt, function () {
+                let size = carouselSlide[0].clientWidth;
+                let counter = this.id;
+                previews.forEach((p) => p.classList.remove(ACTIVE_CLASS));
+                preview.classList.add(ACTIVE_CLASS);
+                carouselSlide.forEach((cl) => {
+                    cl.style.transform = "translateX(" + calculateMovement(size, counter) + "px)";
+                });
             });
         });
     };
